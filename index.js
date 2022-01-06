0ad4c5708f39edb097306020604b74a4e3835bbc
Rashedul Hasan <Rashedul Hasan>
2022-01-06 20:38:39 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+// console.log("Hello World!");
+
+// var num = 5;
+// var numNeed = 6;
+
+// var burgerPrice = 120.4325;
+// console.log(typeof burgerPrice);
+
+// var myName = "Tapu";
+// console.log(typeof myName);
+
+// var isGood = true;
+// var isBad = false;
+// console.log(typeof isGood);
+// console.log(typeof isBad);
+
+// var myLove = "I love Bangladesh";
+// console.log(myLove.toLowerCase());
+// console.log(myLove.toUpperCase());
+// console.log(myLove.indexOf('B'));
+// console.log(myLove.split(" "));
+
+// var num = 10;
+// var num2 = parseInt('5');
+// console.log(num + num2);
+
+// var num = .2;
+// var num2 = .1;
+// var result = num + num2;
+// console.log(result.toFixed(2));
+
+// var firstName = "Rashedul";
+// var lastName = "Hasan";
+// var fullName = firstName+" "+lastName;
+// console.log(fullName);
+
+// var num1 = 10;
+// var num2 = 3;
+// var add = num1+num2; //13
+// var sub = num1-num2; //7
+// var multi = num1*num2; //30
+// var div = num1/num2; //3
+// var remi = num1%num2; //1
+// console.log("Add number is:",add);
+// console.log("Sub number is:",sub);
+// console.log("Multi number is:",multi);
+// console.log("Div number is:",div);
+// console.log("Remi number is:",remi);
+
+
+var number = 5;
+number++;// 6  // number = number+1
+number++;// 7
+number++;// 8
+number--;// 7
+number--;// 6
+console.log(number);
+
+
+
+
+
